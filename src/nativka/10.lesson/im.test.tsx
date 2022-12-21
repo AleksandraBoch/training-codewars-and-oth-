92d4faf279fb0ded1f7b6979ba3b0df9df380ff8
AleksandraBoch <shemaFoxy@gmail.com>
2022-12-21 20:25:52 +0300

    21.12

--- src/nativka/10.lesson/im.test.tsx
+++ src/nativka/10.lesson/im.test.tsx
@@ -1,4 +1,13 @@
-import {hairStyle, moveUser, UserType, UserWithLaptopType} from "./im";
+import {
+    addNewBooks,
+    hairStyle,
+    moveUser, removeBook,
+    updateBook,
+    upgradedLaptop,
+    UserType,
+    UserWithBooksType, UserWithCompaniesType,
+    UserWithLaptopType
+} from "./im";
 
 //поверхностное копирование обьектов
 
@@ -36,3 +45,87 @@ test ('change address',()=>{
 expect(movedUser.address.city).toBe('Kiev')
     expect(user.laptop.title).toBe('Apple')
 })
+
+test('update laptop to macbook',()=>{
+    let user:UserWithLaptopType= {
+        name: 'Alex',
+        hair: 40,
+        address: {
+            city: 'Piter'
+        },
+        laptop:{
+            title:'Apple'
+        }
+    }
+    let upgradedUser=upgradedLaptop(user,'macbook')
+
+    expect(user).not.toBe(upgradedUser)
+    expect(user.address).toBe(upgradedUser.address)
+    expect(upgradedUser.laptop.title).toBe('macbook')
+
+})
+
+test('add new books to user',()=> {
+    let user: UserWithLaptopType & UserWithBooksType = {
+        name: 'Alex',
+        hair: 40,
+        address: {
+            city: 'Piter'
+        },
+        laptop: {
+            title: 'Apple'
+        },
+        books: ['css', 'Js', 'HTML'],
+
+    }
+    let upgradedUser=addNewBooks(user,'ts')
+
+    expect(user).not.toBe(upgradedUser)
+    expect(user.address).toBe(upgradedUser.address)
+    expect(user.books).not.toBe(upgradedUser.books)
+})
+
+
+
+test('update Js to ts',()=> {
+    let user: UserWithLaptopType & UserWithBooksType = {
+        name: 'Alex',
+        hair: 40,
+        address: {
+            city: 'Piter'
+        },
+        laptop: {
+            title: 'Apple'
+        },
+        books: ['css', 'js', 'HTML'],
+
+    }
+    let upgradedUser=updateBook(user,'js','ts')
+
+    expect(user).not.toBe(upgradedUser)
+    expect(user.address).toBe(upgradedUser.address)
+    expect(user.books).not.toBe(upgradedUser.books)
+    expect(upgradedUser.books[1]).toBe('ts')
+})
+
+test('remove Js book',()=> {
+    let user: UserWithLaptopType & UserWithBooksType = {
+        name: 'Alex',
+        hair: 40,
+        address: {
+            city: 'Piter'
+        },
+        laptop: {
+            title: 'Apple'
+        },
+        books: ['css', 'js', 'HTML'],
+
+    }
+    let upgradedUser=removeBook (user,'js')
+
+    expect(user).not.toBe(upgradedUser)
+    expect(user.address).toBe(upgradedUser.address)
+    expect(user.books).not.toBe(upgradedUser.books)
+
+})
+
--- src/nativka/10.lesson/im.tsx
+++ src/nativka/10.lesson/im.tsx
@@ -1,3 +1,5 @@
+import {strict} from "assert";
+
 export type UserType= {
     name: string
     hair: number
@@ -11,6 +13,14 @@ export type LaptopType={
 export type UserWithLaptopType=UserType&{
     laptop:LaptopType
 }
+export type UserWithBooksType=UserType&{
+    books:Array<string>
+}
+
+export type UserWithCompaniesType={
+    companies:Array<{id:number,title:string}>
+}
+
 export function hairStyle(u:UserType,le:number){
     const copy={
         ...u
@@ -31,3 +41,45 @@ export function moveUser(u:UserWithLaptopType,city:string){
 
 return copy
 }
+
+export function upgradedLaptop(u:UserWithLaptopType,title:string){
+    return{
+        ...u,
+        laptop:{
+            title:title
+        }
+    }
+
+}
+
+export function addNewBooks(u:UserWithBooksType&UserWithLaptopType,newBooks:string){
+    //скопировать и добавить элементы
+    return{
+        ...u,
+        books:[...u.books,newBooks]
+    }
+
+}
+
+export function updateBook(u:UserWithBooksType&UserWithLaptopType,oldBook:string,newBook:string){
+//добавить элементы в массив
+    return{
+        ...u,
+        books:u.books.map(b=>{
+            if(b===oldBook){return newBook}
+            else {
+            return b}
+        })
+    }
+}
+
+export function removeBook(u:UserWithBooksType&UserWithLaptopType,removedBook:string){
+    //удаление элемента из массива
+
+    return{
+        ...u,
+        books:u.books.filter(b=>b!==removedBook
+        )
+    }
+}
+
