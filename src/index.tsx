ffbc84cd92ed5aeaaa7359f1a67b5532bb3bf17b
AleksandraBoch <shemaFoxy@gmail.com>
2023-01-12 23:24:28 +0300

    21.12

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,29 @@
 import React from "react";
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
-import reportWebVitals from './reportWebVitals'
 
+type ActionType = {
+    type: "SUM"|"SUB"|"MULT"|"DIV"|"EXP"
+    payload: number
+}
 
+export const calculator = (state: number, action: ActionType): number => {
+    switch (action.type) {
+        case "SUM":
+            return state + action.payload
+        case "SUB":
+            return state - action.payload
+        case "DIV":
+            return state / action.payload
+        case "MULT":
+            return state * action.payload
+        case "EXP":
+            return state ** action.payload
+        default:
+            return state
+    }
+}
+const result = calculator(10, {type:"EXP", payload: 0})
+if (!(result - 1)){
+    console.log("IT-INCUBATOR")
+}
 
-const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
-root.render(
-    <App/>
-)
-
-reportWebVitals()
+//Что надо написать вместо XXX чтобы в консоли появилась строка "IT-INCUBATOR"?
