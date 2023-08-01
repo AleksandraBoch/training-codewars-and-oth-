b50efe9ee0072300a06083245e9926348135217e
AleksandraBoch <shemaFoxy@gmail.com>
2023-08-01 16:52:56 +0300

    codewars

--- src/codewarsTrain/codewars.tsx
+++ src/codewarsTrain/codewars.tsx
@@ -40,12 +40,20 @@ import React from "react";
 //
 // Only valid years (positive integers) will be tested, so you don't have to validate them
 // Examples can be found in the test fixture.
-
-function isLeapYear(year) {
-  if(year % 400 ==0)return true
-  if(year % 100 ==0) return false
-
-   if (year % 4 ==0) return true
-  return false
-  // TODO
-}
+//
+// function isLeapYear(year) {
+//   if(year % 400 ==0)return true
+//   if(year % 100 ==0) return false
+//
+//    if (year % 4 ==0) return true
+//   return false
+//   // TODO
+// }
+// An isogram is a word that has no repeating letters, consecutive or non-consecutive. Implement a function that determines whether a string that contains only letters is an isogram. Assume the empty string is an isogram. Ignore letter case.
+//
+// Example: (Input --> Output)
+//
+// "Dermatoglyphics" --> true "aba" --> false "moOse" --> false (ignore letter case)
+// function isIsogram(str){
+//     return new Set(str.toUpperCase()).size == str.length;
+// }
