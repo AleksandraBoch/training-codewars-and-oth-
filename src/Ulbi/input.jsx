57bea0b7eb88d6e306a860d8ec0bda248fb240f9
AleksandraBoch <shemaFoxy@gmail.com>
2023-09-04 10:58:56 +0300

    preventDefault added

--- src/Ulbi/input.jsx
+++ src/Ulbi/input.jsx
@@ -1,25 +1,31 @@
-import React, {useState} from 'react';
+import React, {useRef, useState} from 'react';
 import Button from "./button";
 import MyInput from "./MyInput";
 
 const Input = (props) => {
 
     const [title, seTitle] = useState('')
+    const [body,setBody]=useState('')
+
     const addNewPost = (e) => {
         //тут удаляется обычная работа формы-те этим самым говорим что
         //покнопке мы не делаем перезагрузку страницы
         e.preventDefault()
        seTitle(e.currentTarget.value)
+
     }
+
     return (
         <div>
             <form>
                 <MyInput
                     value={title}
-                    onChange={addNewPost()}
+                    onChange={addNewPost}
                     type={"text"}
                     placeholder={'post title'} {...props}/>
                 <MyInput
+                    //берем инфу у неконтролируемогоинпута
+                    ref={bodyInputRef}
                     type={"text"}
                     placeholder={'post description'}
                     {...props}/>
@@ -33,3 +39,19 @@ const Input = (props) => {
 };
 
 export default Input;
+//неконтролируемый
+
+
+// const bodyInputRef=useRef()//получаем доступ к Дом э-ту и забираем данные
+
+// <MyInput
+//     //берем инфу у неконтролируемогоинпута
+//     ref={bodyInputRef}
+//     type={"text"}
+//     placeholder={'post description'}
+//     {...props}/>
+//
+// const addPostControledInput=(bodyInputRef)=>{
+//     bodyInputRef.target.value
+//
+// }
