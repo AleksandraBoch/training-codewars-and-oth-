2a5565292942304f7404b8d7674fd091a0983278
AleksandraBoch <shemaFoxy@gmail.com>
2024-03-04 09:39:49 +0300

    cw

---
+++ src/livecof=d/sd.ts
@@ -0,0 +1,38 @@
+const users = [
+    {id: 1, name: "Ivan", city: "Msc"},
+    {id: 2, name: "Ivan", city: "Spb"},
+    {id: 3, name: "Alexey", city: "Ekb"},
+    {id: 4, name: "Vlad", city: "Spb"},
+    {id: 5, name: "Elena", city: "Msc"}
+]
+
+
+const groupBy = (arr, key) => {
+    const result = {}
+
+    arr.forEach((item) => {
+        item =   {id: 5, name: "Elena", city: "Msc"}
+
+        const keyValue = item[key]  // Msc
+
+        if (result[keyValue]) {
+            result[keyValue].push(item)
+        } else {
+            result[keyValue] = [item]
+        }
+    })
+
+    return result;
+}
+
+groupBy(users, "city")
+
+
+/*
+  {
+    Msc: [{id: 1, name: "Ivan", city: "Msc"}, {id: 5, name: "Elena", city: "Msc"}],
+    Spb: [{id: 2, name: "Ivan", city: "Spb"}, {id: 4, name: "Vlad", city: "Spb"}],
+    Ekb: [{id: 3, name: "Alexey", city: "Ekb"}],
+  }
+*/
+
