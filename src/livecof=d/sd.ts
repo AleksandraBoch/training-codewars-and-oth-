3578002a0aba8c10abd672e0b5416b5c49ec2eb8
AleksandraBoch <shemaFoxy@gmail.com>
2024-09-09 13:33:31 +0300

    cw

--- src/livecof=d/sd.ts
+++ src/livecof=d/sd.ts
@@ -1,31 +1,32 @@
-const users = [
-    {id: 1, name: "Ivan", city: "Msc"},
-    {id: 2, name: "Ivan", city: "Spb"},
-    {id: 3, name: "Alexey", city: "Ekb"},
-    {id: 4, name: "Vlad", city: "Spb"},
-    {id: 5, name: "Elena", city: "Msc"}
-]
-
-
-const groupBy = (arr, key) => {
-    const result = {}
-
-    arr.forEach((item) => {
-        item =   {id: 5, name: "Elena", city: "Msc"}
-
-        const keyValue = item[key]  // Msc
-
-        if (result[keyValue]) {
-            result[keyValue].push(item)
-        } else {
-            result[keyValue] = [item]
-        }
-    })
-
-    return result;
-}
-
-groupBy(users, "city")
+import React from "react";
+// const users = [
+//     {id: 1, name: "Ivan", city: "Msc"},
+//     {id: 2, name: "Ivan", city: "Spb"},
+//     {id: 3, name: "Alexey", city: "Ekb"},
+//     {id: 4, name: "Vlad", city: "Spb"},
+//     {id: 5, name: "Elena", city: "Msc"}
+// ]
+//
+//
+// const groupBy = (arr, key) => {
+//     const result = {}
+//
+//     arr.forEach((item) => {
+//         item =   {id: 5, name: "Elena", city: "Msc"}
+//
+//         const keyValue = item[key]  // Msc
+//
+//         if (result[keyValue]) {
+//             result[keyValue].push(item)
+//         } else {
+//             result[keyValue] = [item]
+//         }
+//     })
+//
+//     return result;
+// }
+//
+// groupBy(users, "city")
 
 
 /*
